832dbb8677994005fb4bca6cba136378887a1a1b
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-05 14:21:16 +1000

    Make reverseString function return  reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    reversedString = '';
+    for (let i = inputString.length - 1; i >= 0; i--){
+        reversedString += inputString[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
