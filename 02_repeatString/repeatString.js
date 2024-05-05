db658bfcf900abf88406cce800b95aee5c4847e3
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-05 14:07:31 +1000

    JS file modified to pass the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    tempStr = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++){
+        tempStr += string;
+    }
+    return tempStr;
 };
 
 // Do not edit below this line
