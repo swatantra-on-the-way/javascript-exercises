61606d4523f232dcae9bb06c9c9002cae5355f3a
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-05 23:10:32 +1000

    Change calculator js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumOfArr = 0;
+  for (let i = 0; i < arr.length; ++i){
+    sumOfArr += arr[i];
+  }
+  return sumOfArr;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let mult = 1;
+  for (let i = 0; i < arr.length; ++i){
+    mult *= arr[i];
+  }
+  return mult;
 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  let fact = 1;
+  while (input >= 1) {
+    fact *= input;
+    input--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
