d3f2542347164eaf07240fae06a50e513690efd2
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-05 22:55:34 +1000

    Change temperature conversion js file

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  result = ((fTemp - 32) * (10 / 18));
+  tmp1 = Number(result.toFixed());
+  if (result - tmp1 === 0){
+    return result;
+  }
+  return Number(result.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  result = ((18 / 10) * cTemp + 32);
+  tmp1 = Number(result.toFixed());
+  if (result - tmp1 === 0){
+    return result;
+  }
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
