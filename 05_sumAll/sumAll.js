2fd10bede2dca8b5f4453a238a34933b361207c7
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-05 20:30:43 +1000

    Change leapYears.js file to check leap year occurrence

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = (start, end) => {
+    if (typeof(start) != 'number' || typeof(end) != 'number' || start < 0 || end < 0){
+        return 'ERROR';
+    }
+    let sum = 0;
+    if (start > end){
+        let tmp;
+        tmp = start;
+        start = end;
+        end = tmp;
+    }
+    for (let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
